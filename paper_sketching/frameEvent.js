b2144366cb174d817b9b1e9f3611c31d6e299363
idealeah <willl973@newschool.edu>
2020-01-23 14:17:14 -0500

    yes

---
+++ paper_sketching/frameEvent.js
@@ -0,0 +1,22 @@
+//code from Justin Bakse's Comp Form class
+var bounds = new Rectangle(new Point(0, 0), new Point(200, 200));
+var rectangle = new Path.Rectangle(bounds);
+rectangle.fillColor = '#333';
+
+
+var circle1 = new Path.Circle([100, 100], 10);
+circle1.fillColor = 'red';
+var deltaPosition = new Point(5, 0);
+
+function onFrame() {
+    circle1.position += deltaPosition;
+    if (circle1.position.x > 190) {
+        deltaPosition.x = -Math.abs(deltaPosition.x);
+        circle1.fillColor = 'green';
+    }
+    if (circle1.position.x < 10) {
+        deltaPosition.x = Math.abs(deltaPosition.x);
+        circle1.fillColor = 'blue';
+    }
+    console.log(circle1);
+}
